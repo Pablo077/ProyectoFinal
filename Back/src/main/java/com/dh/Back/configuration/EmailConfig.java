4d9fec7e0f6d833285e345e97e31472f37929db6
Pablo Ballatore <pfb1983@outlook.com>
2025-05-27 18:12:01 -0300

    Se logra hacer funcionar correo

--- Back/src/main/java/com/dh/Back/configuration/EmailConfig.java
+++ Back/src/main/java/com/dh/Back/configuration/EmailConfig.java
@@ -12,6 +12,11 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.SSLContext;
+import java.security.cert.X509Certificate;
+
 @Configuration
 @PropertySource("classpath:email.properties")
 public class EmailConfig {
@@ -27,6 +32,20 @@ public class EmailConfig {
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.host", "smtp.gmail.com");
         properties.put("mail.smtp.port", "587");
+
+        // Ignorar validación SSL
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                public X509Certificate[] getAcceptedIssuers() { return null; }
+            }}, null);
+            SSLContext.setDefault(sslContext);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return properties;
     }
 
